456759275c78e58b49b8391fd88fae0f4a1182e1
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-08-20 16:06:59 +0100

    added exception handler for restful responses

--- src/main/java/com/postitapplications/user/exception/UserExceptionHandler.java
+++ src/main/java/com/postitapplications/user/exception/UserExceptionHandler.java
@@ -1,10 +1,32 @@
 package com.postitapplications.user.exception;
 
+import com.postitapplications.exception.ExceptionResponseBody;
+import com.postitapplications.exception.exceptions.NullOrEmptyException;
+import com.postitapplications.exception.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class UserExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(value = {NullOrEmptyException.class, NullPointerException.class})
+    public ResponseEntity<Object> handleBadRequestException(Exception exception) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(badRequest,
+            exception.getMessage());
 
+        return new ResponseEntity<>(exceptionResponseBody, badRequest);
+    }
+
+    @ExceptionHandler(value = {UserNotFoundException.class})
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception) {
+        HttpStatus notFound = HttpStatus.NOT_FOUND;
+        ExceptionResponseBody exceptionResponseBody = new ExceptionResponseBody(notFound,
+            exception.getMessage());
+
+        return new ResponseEntity<>(exceptionResponseBody, notFound);
+    }
 }
