3a451bd2ed60ace9da888a2bfb42c1a9c18dd26a
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-08-24 17:03:35 +0100

    finished integration tests

--- src/test/java/com/postitapplications/user/integrationTest/UserIT.java
+++ src/test/java/com/postitapplications/user/integrationTest/UserIT.java
@@ -13,7 +13,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -66,8 +70,7 @@ public class UserIT {
 
     @Test
     public void saveUserShouldReturnUnsupportedMediaTypeStatusCodeWhenUserIsNull() {
-        ResponseEntity<User> responseEntity = restTemplate
-            .postForEntity("/user", null, User.class);
+        ResponseEntity<User> responseEntity = restTemplate.postForEntity("/user", null, User.class);
         HttpStatus responseStatusCode = responseEntity.getStatusCode();
 
         assertThat(responseStatusCode).isEqualTo(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
@@ -122,8 +125,162 @@ public class UserIT {
 
     @Test
     public void getUserByIdShouldReturnBadRequestStatusCodeOnInvalidUserId() {
+        ResponseEntity<User> responseEntity = restTemplate.getForEntity("/user/123456", User.class);
+        HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
+
+        assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void updateUserShouldReturnOKStatusCodeOnSuccessfulUpdate() {
+        UUID savedUserId = UUID.randomUUID();
+        User savedUser = new User(savedUserId, "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        User updatedUser = new User(savedUserId, "johnSmith456", "password");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> httpEntity = new HttpEntity<>(updatedUser, headers);
+
         ResponseEntity<User> responseEntity = restTemplate
-            .getForEntity("/user/123456", User.class);
+            .exchange("/user/", HttpMethod.PUT, httpEntity, User.class);
+        HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
+
+        assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void updateUserShouldReturnUpdatedUserOnSuccessfulUpdate() {
+        UUID savedUserId = UUID.randomUUID();
+        User savedUser = new User(savedUserId, "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        User updatedUser = new User(savedUserId, "johnSmith456", "password");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> httpEntity = new HttpEntity<>(updatedUser, headers);
+
+        ResponseEntity<User> responseEntity = restTemplate
+            .exchange("/user/", HttpMethod.PUT, httpEntity, User.class);
+        User responseEntityBody = responseEntity.getBody();
+
+        assertThat(responseEntityBody.getUsername()).isEqualTo("johnSmith456");
+        assertThat(responseEntityBody.getPassword()).isEqualTo("password");
+    }
+
+    @Test
+    public void updateUserShouldUpdateUserOnSuccessfulUpdate() {
+        UUID savedUserId = UUID.randomUUID();
+        User savedUser = new User(savedUserId, "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        User updatedUser = new User(savedUserId, "johnSmith456", "password");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> httpEntity = new HttpEntity<>(updatedUser, headers);
+
+        restTemplate.exchange("/user/", HttpMethod.PUT, httpEntity, User.class);
+        User userToTest = mongoTemplate.findAll(User.class).get(0);
+
+        assertThat(userToTest.getUsername()).isEqualTo("johnSmith456");
+    }
+
+    @Test
+    public void updateUserShouldReturnNotFoundStatusCodeOnUserWithNonExistingUserId() {
+        User savedUser = new User(UUID.randomUUID(), "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        User updatedUser = new User(UUID.randomUUID(), "johnSmith456", "password");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> httpEntity = new HttpEntity<>(updatedUser, headers);
+
+        ResponseEntity<User> responseEntity = restTemplate
+            .exchange("/user/", HttpMethod.PUT, httpEntity, User.class);
+        HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
+
+        assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void updateUserShouldReturnBadRequestStatusCodeOnUserWithInvalidFields() {
+        User updatedUser = new User(UUID.randomUUID(), null, "password");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> httpEntity = new HttpEntity<>(updatedUser, headers);
+
+        ResponseEntity<User> responseEntity = restTemplate
+            .exchange("/user/", HttpMethod.PUT, httpEntity, User.class);
+        HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
+
+        assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void deleteUserByIdShouldReturnOkStatusCodeOnSuccessfulDelete() {
+        UUID savedUserId = UUID.randomUUID();
+        User savedUser = new User(savedUserId, "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<UUID> responseEntity = restTemplate
+            .exchange("/user/" + savedUserId.toString(), HttpMethod.DELETE, httpEntity, UUID.class);
+        HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
+
+        assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void deleteUserByIdShouldReturnDeletedUsersIdOnSuccessfulDelete() {
+        UUID savedUserId = UUID.randomUUID();
+        User savedUser = new User(savedUserId, "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<UUID> responseEntity = restTemplate
+            .exchange("/user/" + savedUserId.toString(), HttpMethod.DELETE, httpEntity, UUID.class);
+        UUID responseEntityBody = responseEntity.getBody();
+
+        assertThat(responseEntityBody).isEqualTo(savedUserId);
+    }
+
+    @Test
+    public void deleteUserByIdShouldDeleteUserOnSuccessfulDelete() {
+        UUID savedUserId = UUID.randomUUID();
+        User savedUser = new User(savedUserId, "johnSmith123", "password");
+        mongoTemplate.save(savedUser);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        restTemplate
+            .exchange("/user/" + savedUserId.toString(), HttpMethod.DELETE, httpEntity, UUID.class);
+
+        assertThat(mongoTemplate.findAll(User.class).size()).isEqualTo(0);
+    }
+
+    @Test
+    public void deleteUserByIdShouldReturnNotFoundStatusCodeWithNonExistingUserId() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> responseEntity = restTemplate
+            .exchange("/user/" + UUID.randomUUID().toString(), HttpMethod.DELETE, httpEntity,
+                String.class);
+        HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
+
+        assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void deleteUserByIdShouldReturnBadRequestStatusCodeWithInvalidUserId() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> responseEntity = restTemplate
+            .exchange("/user/123456", HttpMethod.DELETE, httpEntity, String.class);
         HttpStatus responseEntityStatusCode = responseEntity.getStatusCode();
 
         assertThat(responseEntityStatusCode).isEqualTo(HttpStatus.BAD_REQUEST);
